286739fad82dd0149217ff2d25c8eb67a5f200c0
kamran alam <kammykamran0093@gmail.com>
2026-03-19 23:22:40 +0530

    Day 6: Update Vite config for proxy and build optimization

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'https://slrtech-chatapp.onrender.com/',
+        target:'http://localhost:3000',
+        changeOrigin: true,
         secure:false
       }
     }
