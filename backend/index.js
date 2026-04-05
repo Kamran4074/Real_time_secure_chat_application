16ce6e8ef0b0771600be8c6b61102cd783d085ac
kamran alam <kammykamran0093@gmail.com>
2026-04-05 15:19:34 +0530

    Fix CORS to allow Vercel production origin

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,14 @@ const __dirname = path.resolve();
 dotenv.config();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://chatwave-azure.vercel.app'],
+    origin: function(origin, callback) {
+        const allowedOrigins = ['http://localhost:5173', 'https://chatwave-azure.vercel.app'];
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
 app.use(express.json());
